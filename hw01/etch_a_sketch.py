296d19bdbdad4a06ce8a3e4e65a66d9be88140a9
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-08 21:44:13 -0400

    added a board which holds drawn values

--- hw01/etch_a_sketch.py
+++ hw01/etch_a_sketch.py
@@ -6,14 +6,25 @@ import time
 import curses 
 from curses import wrapper
 
-max_height = 8
-max_width = 8
+max_height = 9
+max_width = 9
 
-def drawscreen(screen):
-	screen.refresh()
+def drawscreen(screen, sketch, pen_position):
+    screen.clear()
+    for i in range(9):
+        for j in range(9):
+            screen.addch(i*2, j*3, sketch[j][i])
+    screen.refresh()
+	
+
+def clearscreen(screen):
+    sketch = [[' ' for i in range(max_height)] for j in range(max_width)]
+    screen.clear()
+    screen.refresh()
+    return sketch
 
 def read_key(screen, pen_position):
-	key_code = screen.getch()
+	key_code = screen.getkey()
 	if key_code == 'KEY_DOWN':
 		if pen_position[1] > 0:
 			pen_position = [pen_position[0], pen_position[1]-1]
@@ -27,18 +38,23 @@ def read_key(screen, pen_position):
 		if pen_position[0] > 0:
 			pen_position = [pen_position[0]-1, pen_position[1]]
 	if key_code == ' ':
-		screen.clear()
-		screen.refresh()
+		clearscreen(screen)
+	return pen_position;
 
 def main(screen):
 	screen.addstr("Welcome to the game Etch-A-Sketch! To begin use the arrow keys to direct the \npen on the screen. When you want to clear the screen press the space bar to \nshake the Etch-A-Sketch. Have fun!")
 	screen.refresh()
-	curses.napms(2000)
-	screen.clear()
-	pen_position = [0,0]
+	curses.napms(5000)
+	
+	sketch = clearscreen(screen)
+	pen_position = [4,4]
+	
 	while(1):
-		read_key(screen, pen_position)
-		drawscreen(screen)
+		pen_position = read_key(screen, pen_position)
+		sketch[pen_position[0]][pen_position[1]] = 'x'
+		drawscreen(screen, sketch, pen_position)
+		
+		curses.napms(500)
 
 screen = curses.initscr()
 main(screen)
