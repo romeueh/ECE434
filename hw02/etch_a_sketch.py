c4a711e634ab54d63fcf982d5b67c393c4dd63a6
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-15 16:54:04 -0400

    Update etch_a_sketch.py

--- hw02/etch_a_sketch.py
+++ hw02/etch_a_sketch.py
@@ -78,11 +78,11 @@ def main(screen):
 		if (GPIO.event_detected(button_down)) and (pen_position[1] < max_dim-1):
 			pen_position = [pen_position[0], pen_position[1]+1]
 		if (GPIO.event_detected(button_up)) and ( pen_position[1] > 1):
-			changed_pen_position = [pen_position[0], pen_position[1]-1]
+			pen_position = [pen_position[0], pen_position[1]-1]
 		if (GPIO.event_detected(button_right)) and (pen_position[0] < max_dim-1):
-			changed_pen_position = [pen_position[0]+1, pen_position[1]]
+			pen_position = [pen_position[0]+1, pen_position[1]]
 		if (GPIO.event_detected(button_left)) and (pen_position[0] > 1):
-			changed_pen_position = [pen_position[0]-1, pen_position[1]]
+			pen_position = [pen_position[0]-1, pen_position[1]]
 		if GPIO.event_detected(button_shake):
 			shake = True
 		if GPIO.event_detected(button_exit):
