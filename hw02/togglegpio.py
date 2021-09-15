5173c9c9e9a4d0af22c9ad1e379fb58ff4a328c1
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-15 11:00:12 -0400

    added period variable

--- hw02/togglegpio.py
+++ hw02/togglegpio.py
@@ -3,12 +3,14 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
+period = 0.1; 
+
 LED1 = "P9_12"
 GPIO.setup(LED1, GPIO.OUT)
 
 while(1):
     GPIO.output(LED1, GPIO.LOW)
-    time.sleep(0.005)
+    time.sleep(period/2)
         
     GPIO.output(LED1, GPIO.HIGH)
-    time.sleep(0.005)
+    time.sleep(period/2)
