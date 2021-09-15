84b47b732148786cb3c49abe632e06cfc9b8644f
Demo User <debian@beaglebone.localdomain>
2021-09-15 03:20:34 +0000

    program now opens

--- hw02/etch_a_sketch.py
+++ hw02/etch_a_sketch.py
@@ -1,4 +1,4 @@
-!/usr/bin/env python3
+#!/usr/bin/env python3
 #chmod +x etch_a_sketch.py
 #Author: Eliza Romeu
 
@@ -7,10 +7,15 @@ import curses
 import Adafruit_BBIO.GPIO as GPIO
 from curses import wrapper
 
-screen = curses.initscr()
 global exit
-global clear
 global pen_position
+global max_dim
+
+
+clear = False
+exit = False
+pen_position = [1,1]
+max_dim = 5
 
 button_exit = "P9_13"
 button_clear = "P9_14"
@@ -26,13 +31,15 @@ GPIO.setup(button_right, GPIO.IN)
 GPIO.setup(button_up, GPIO.IN)
 GPIO.setup(button_down, GPIO.IN)
 
+screen = curses.initscr()
+
 def drawscreen(screen, sketch):
     screen.clear()
     for i in range(max_dim):
         for j in range(max_dim):
             screen.addch(i*2, j*3, sketch[j][i])
     screen.refresh()
-    
+
 def clearscreen(screen):
     sketch = [[' ' for i in range(max_dim)] for j in range(max_dim)]
     for i in range(max_dim):
@@ -59,38 +66,32 @@ def read_button(channel):
                         if pen_position[0] > 1:
                                 pen_position = [pen_position[0]-1, pen_position[1]]
                 if button_key == button_clear:
-                        clear = True
+                        sketch = clearscreen(screen)
                 if button_key == button_exit:
                         exit = True
 
 def main(screen):
-        screen.addstr("Welcome to the game Etch-A-Sketch! To begin use the arrow keys to direct the \npen on the screen. When you want to clear the screen press the space bar to \nshake the Etch-A-Sketc$
+        screen.addstr("Welcome to the game Etch-A-Sketch! To begin use the arrow keys to direct the \npen on the screen. When you want to clear the screen press the space bar to \nshake the Etch-A-Sketch. Lastly, press q when you want to exit. Have fun!\n")
         screen.addstr("\nWhat size would you like to board to be 1-9?")
         screen.refresh()
 
-        clear = False
-        exit = False
-        pen_position = [1,1]
         max_dim = int(screen.getch())-47
 
-        sketch = clearscreen(screen, max_dim)
+        sketch = clearscreen(screen)
 
         while(1):
-                drawscreen(screen, sketch, pen_position, max_dim)
-                if clear == True:
-                        sketch = clearscreen(screen, max_dim)
-                        clear = False
+                drawscreen(screen, sketch)
                 if exit == True:
                         break
                 sketch[pen_position[0]][pen_position[1]] = 'x'
                 curses.napms(100)
 
-GPIO.add_event_detect(button_exit, GPIO.RISING, callback=read_button)
-GPIO.add_event_detect(button_clear, GPIO.RISING, callback=read_button)
-GPIO.add_event_detect(button_left, GPIO.RISING, callback=read_button)
-GPIO.add_event_detect(button_right, GPIO.RISING, callback=read_button)
-GPIO.add_event_detect(button_up, GPIO.RISING, callback=read_button)
-GPIO.add_event_detect(button_up, GPIO.RISING, callback=read_button)
+GPIO.add_event_detect(button_exit, GPIO.FALLING, callback=read_button)
+GPIO.add_event_detect(button_clear, GPIO.FALLING, callback=read_button)
+GPIO.add_event_detect(button_left, GPIO.FALLING, callback=read_button)
+GPIO.add_event_detect(button_right, GPIO.FALLING, callback=read_button)
+GPIO.add_event_detect(button_up, GPIO.FALLING, callback=read_button)
+GPIO.add_event_detect(button_down, GPIO.FALLING, callback=read_button)
 
 curses.wrapper(main)
 
