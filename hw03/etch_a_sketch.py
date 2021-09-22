2ccecbaaf3216829e15e5e38b9b4fd5037fced22
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-22 15:22:28 -0400

    Update etch_a_sketch.py

--- hw03/etch_a_sketch.py
+++ hw03/etch_a_sketch.py
@@ -55,7 +55,7 @@ def main(screen):
 	rotary_horizontal_position = encoder2.position
 
 	while(1):
-		if (rotary_vertical_position < encoder1.position) and (pen_position[1] < max_dim-1):
+		if (rotary_vertical_position < encoder1.position) and (pen_position[1] < 8-1):
 			pen_position = [pen_position[0], pen_position[1]+1]
 			rotary_vertical_position = encoder1.position
 			pos_changed = True
@@ -63,7 +63,7 @@ def main(screen):
 			pen_position = [pen_position[0], pen_position[1]-1]
 			rotary_vertical_position = encoder1.position
 			pos_changed = True
-		if (rotary_horizontal_position > encoder2.position) and (pen_position[0] < max_dim-1):
+		if (rotary_horizontal_position > encoder2.position) and (pen_position[0] < 8-1):
 			pen_position = [pen_position[0]+1, pen_position[1]]
 			rotary_horizontal_position = encoder2.position
 			pos_changed = True
