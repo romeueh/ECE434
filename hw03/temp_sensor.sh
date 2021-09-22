b5929a0a0c5b60e6b2c3272c57f1f0dd7edfc6ad
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-21 21:42:21 -0400

    Update temp_sensor.sh

--- hw03/temp_sensor.sh
+++ hw03/temp_sensor.sh
@@ -3,10 +3,10 @@ i2cset -y 2 0x49 0x03 0x16
 
 temp1=`i2cget -y 2 0x48` #retrieve temp from sensor 1
 
-temp1f=$(( ($temp1*9/5) +32)) #converting to Fahrenheit
+temp1f=$((("${temp1}" * 9 / 5 ) + 32)) #converting to Fahrenheit
 echo $temp1f
 
 temp2=`i2cget -y 2 0x49` #retrieve temp from sensor 2
 
-temp2f=$(( ($temp2*9/5) +32)) #converting to Fahrenheit
+temp2f=$((("${temp2}" * 9 / 5 ) + 32)) #converting to Fahrenheit
 echo $temp2f
