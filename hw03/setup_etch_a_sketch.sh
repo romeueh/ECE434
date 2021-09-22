a4d96cadf6213cb8c63d1f0359492d621dd51877
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-22 01:56:29 -0400

    Update setup_etch_a_sketch.sh

--- hw03/setup_etch_a_sketch.sh
+++ hw03/setup_etch_a_sketch.sh
@@ -1,7 +1,7 @@
-echo eqep1: P8.33, P8.35
-config-pin P8.33 qep 
-config-pin P8.35 qep
+echo eqep1: P8_33, P8_35
+config-pin P8_33 eqep 
+config-pin P8_35 eqep
 
-echo eqep2: P8.11, P8.12
-config-pin P8.11 qep 
-config-pin P8.12 qep 
+echo eqep2: P8_11, P8_12
+config-pin P8_11 eqep 
+config-pin P8_12 eqep
