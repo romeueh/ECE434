c4efa95b329db1f8eeee3da43026ae4c3c36602c
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-29 13:34:08 -0400

    Update and rename setup_etch_a_sketch.sh to setup.sh

---
+++ hw04/setup.sh
@@ -0,0 +1,9 @@
+echo i2c2: P9_24, P9_26
+config-pin P9_24 i2c 
+config-pin P9_26 i2c
+
+echo spi0: P9_17, P9_18, P9_21, P9_22
+config-pin P9_17 spi_cs  
+config-pin P9_18 spi  
+config-pin P9_21 spi  
+config-pin P9_22 spi_sclk  
--- hw04/setup_etch_a_sketch.sh
+++
@@ -1,3 +0,0 @@
-echo i2c2: P9_24, P9_26
-config-pin P9_24 i2c 
-config-pin P9_26 i2c
