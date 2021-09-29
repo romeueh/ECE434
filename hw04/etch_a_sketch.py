658ed1392eb706ecc5c70bd3271b2b30ead4f78e
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-29 14:34:32 -0400

    Update etch_a_sketch.py

--- hw04/etch_a_sketch.py
+++ hw04/etch_a_sketch.py
@@ -21,22 +21,26 @@ bus.write_byte_data(matrix, 0xe7, 0)
 sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
 bus.write_i2c_block_data(matrix, 0, sketch)
 
+@app.route("/")
+def index():
+	return render_template('index.html')
+
 @app.route("/<action>")
 def action(action):
 	global pen_position
 	global sketch
 	pos_changed = True
 
-	if (action == "down" and pen_position[1] < 8):
+	if (action == "up" and pen_position[1] < 8):
 		pen_position = [pen_position[0], pen_position[1]+1]
 		pos_changed = True
-	if (action == "up" and pen_position[1] > 1):
+	if (action == "down" and pen_position[1] > 1):
 		pen_position = [pen_position[0], pen_position[1]-1]
 		pos_changed = True
-	if (action == "right" and pen_position[0] < 8-1):
+	if (action == "left" and pen_position[0] < 8-1):
 		pen_position = [pen_position[0]+1, pen_position[1]]
 		pos_changed = True
-	if (action == "left" and pen_position[0] > 0):
+	if (action == "right" and pen_position[0] > 0):
 		pen_position = [pen_position[0]-1, pen_position[1]]
 		pos_changed = True
 	if (action == "clear"):
