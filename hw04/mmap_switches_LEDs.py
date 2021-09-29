1615bdcb74cc1c5392ff8206bc01198910211f63
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-28 22:56:17 -0400

    Update mmap_switches_LEDs.py

--- hw04/mmap_switches_LEDs.py
+++ hw04/mmap_switches_LEDs.py
@@ -21,7 +21,7 @@ LED1 = 1<<17
 button1 = 1<<14
 
 with open("/dev/mem", "r+b" ) as f:
-  mem0 = mmap(f.fileno(), GPIO0_size, offset=GPIO0_startaddrt)
+  mem0 = mmap(f.fileno(), GPIO0_size, offset=GPIO0_startaddr)
   mem1 = mmap(f.fileno(), GPIO1_size, offset=GPIO1_startaddr)
 
 packed_reg0 = mem0[GPIO_OE:GPIO_OE+4]
