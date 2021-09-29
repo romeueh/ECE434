990712d7303c3b6cc8cc1ce100f890ce56fa5394
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-29 14:16:37 -0400

    Update etch_a_sketch.py

--- hw04/etch_a_sketch.py
+++ hw04/etch_a_sketch.py
@@ -9,27 +9,17 @@ import Adafruit_BBIO.GPIO as GPIO
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-global pen_position
-global sketch
-pen_position = [1,2]
-
 bus = smbus.SMBus(1)
 matrix = 0x70
+sketch = [0x00 for i in range(16)]
+pen_position = [1,2]
 
 bus.write_byte_data(matrix, 0x21, 0)
 bus.write_byte_data(matrix, 0x81, 0)
 bus.write_byte_data(matrix, 0xe7, 0)
 
-def drawscreen(sketch, pen_position):
-	sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
-	bus.write_i2c_block_data(matrix, 0, sketch)
-
-def clearscreen():
-	sketch = [0x00 for i in range(16)]
-	return sketch
-
-sketch = clearscreen()
-drawscreen(sketch, pen_position)
+sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
+bus.write_i2c_block_data(matrix, 0, sketch)
 
 @app.route("/<deviceName>/<action>")
 def action(deviceName, action):
@@ -50,16 +40,17 @@ def action(deviceName, action):
 		pen_position = [pen_position[0]-1, pen_position[1]]
 		pos_changed = True
 	if (action == "clear"):
-		sketch = clearscreen()
+		sketch = [0x00 for i in range(16)]
 		pos_changed = True
 	#if GPIO.event_detected(button_exit):
 		#break
 			
 	if(pos_changed):
-		drawscreen(sketch, pen_position)
+		sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
+		bus.write_i2c_block_data(matrix, 0, sketch)
 		pos_changed = False
 			
-	return render_template('index.html', **templateData)
+	return render_template('index.html')
 
 if __name__ == "__main__":
         app.run(debug=True, port=8081, host='0.0.0.0')
