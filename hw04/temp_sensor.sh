918a17d41bbe4056ed1bfb363ae66c0f9b72e6ff
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-28 22:54:44 -0400

    Update temp_sensor.sh

--- hw04/temp_sensor.sh
+++ hw04/temp_sensor.sh
@@ -4,6 +4,6 @@ echo i2c1: P9_24, P9_26
 config-pin P9_24 i2c 
 config-pin P9_26 i2c
 
-echo Reading from TMP101 Sensor (0x49)
+echo Reading from TMP101 Sensor - 0x49
 cd /sys/class/i2c-adapter/i2c-2/2-0049/hwmon/hwmon0
 cat temp1_input 
