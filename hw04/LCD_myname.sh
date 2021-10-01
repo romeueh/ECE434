c819647bb73086a727954a8bfc2b6359541b5d78
romeueh <75344382+romeueh@users.noreply.github.com>
2021-09-30 22:34:21 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -6,7 +6,8 @@ TMP_FILE=/tmp/frame.png
 # From: http://www.imagemagick.org/Usage/text/
 convert -background lightblue -fill blue -font Times-Roman -pointsize 24 \
      -size $SIZE \
-     -draw "text 0,100 'Eliza Romeu'" \
+     label:'ImageMagick Usage' \
+     -draw "text 0,200 'Eliza Romeu'" \
      $TMP_FILE
 
 sudo fbi -noverbose -T 1 $TMP_FILE
