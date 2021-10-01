cfd1014f4ff312009fba13792882744781d856b2
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-01 15:11:25 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -1,7 +1,7 @@
 # Here's how to use imagemagick to display text
 # Make a blank image
 SIZE=320x240
-TMP_FILE=/tmp/frame.png
+TMP_FILE=boris_ref_iamge.png
 
 # From: http://www.imagemagick.org/Usage/text/
 convert -background lightblue -fill blue -font Times-Roman -pointsize 24 \
