332ffa4ba9bb7cc99b7ac8c95a02ee06512f46ce
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-01 15:41:00 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -3,12 +3,8 @@
 SIZE=320x240
 TMP_FILE=boris_ref_iamge.png
 
-# From: http://www.imagemagick.org/Usage/text/
-convert -font Times-Roman -pointsize 24 \
-     -size $SIZE \
-     label:'ImageMagick Usage' \
-     -draw "text 0,200 'Eliza Romeu'" \
-     $TMP_FILE
+convert TMP_FILE -resize $SIZE TMP_FILE
+convert TMP_FILE -rotate 0 TMP_FILE
+convert TMP_FILE -gravity NorthWest -annotate 0 'Eliza Romeu'  TMP_FILE
 
-convert $TMP_FILE -rotate 0 $TMP_FILE
-sudo fbi -noverbose -T 1 $TMP_FILE
+sudo fbi -noverbose -T 1 -a $TMP_FILE
