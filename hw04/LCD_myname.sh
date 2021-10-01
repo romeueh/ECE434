8a5656a9105720d34ea86d7ea1cfdbe6be1e66b1
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-01 15:54:01 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -4,7 +4,7 @@ SIZE=320x240
 TMP_FILE=boris_ref_iamge.png;
 
 convert $TMP_FILE -resize $TMP_FILE
-convert $TMP_FILE -rotate 90 $TMP_FILE
+convert $TMP_FILE -rotate 0 $TMP_FILE
 convert $TMP_FILE -gravity NorthWest -annotate 0 'Eliza Romeu'  $TMP_FILE
 
 sudo fbi -noverbose -T 1 -a $TMP_FILE
