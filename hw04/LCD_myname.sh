2ae21b79d75e1ff5392dd48b5f9a5737436dc2cc
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-01 15:50:13 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -1,8 +1,9 @@
 # Here's how to use imagemagick to display text
 # Make a blank image
 SIZE=320x240
+TMP_FILE=boris_ref_iamge.png;
 
-convert boris_ref_iamge.png -resize $TMP_FILE
+convert $TMP_FILE -resize $TMP_FILE
 convert $TMP_FILE -rotate 90 $TMP_FILE
 convert $TMP_FILE -gravity NorthWest -annotate 0 'Eliza Romeu'  $TMP_FILE
 
