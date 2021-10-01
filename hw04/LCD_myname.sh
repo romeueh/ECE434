96fe4e34ce95c90fb28cc79b87d85eca75731684
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-01 15:47:30 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -3,7 +3,7 @@
 SIZE=320x240
 TMP_FILE=/tmp/frame.png
 
-convert $TMP_FILE -resize $SIZE boris_ref_iamge.png
+convert boris_ref_iamge.png -resize $TMP_FILE
 convert $TMP_FILE -rotate 90 $TMP_FILE
 convert $TMP_FILE -gravity NorthWest -annotate 0 'Eliza Romeu'  $TMP_FILE
 
