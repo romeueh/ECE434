cbc3422d534e39e62fbb1d93cbbd9279ca017e14
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-01 15:34:50 -0400

    Update LCD_myname.sh

--- hw04/LCD_myname.sh
+++ hw04/LCD_myname.sh
@@ -4,7 +4,7 @@ SIZE=320x240
 TMP_FILE=boris_ref_iamge.png
 
 # From: http://www.imagemagick.org/Usage/text/
-convert -background lightblue -fill blue -font Times-Roman -pointsize 24 \
+convert -fill lightblue -font Times-Roman -pointsize 24 \
      -size $SIZE \
      label:'ImageMagick Usage' \
      -draw "text 0,200 'Eliza Romeu'" \
