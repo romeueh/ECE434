437d703b49ecf4bf4f68cff2c37889b7d3902c15
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-11 09:50:24 -0400

    Update etch_a_sketch.c

--- hw05/etch_a_sketch/etch_a_sketch.c
+++ hw05/etch_a_sketch/etch_a_sketch.c
@@ -31,28 +31,70 @@ MODULE_VERSION("0.1");
 #define sketch [0x00 for i in range(16)]
 #define pen_position [1,2]
 
-int main(int argc, char **argv) {
-	bus.write_byte_data(matrix, 0x21, 0)
-	bus.write_byte_data(matrix, 0x81, 0)
-	bus.write_byte_data(matrix, 0xe7, 0)
+void i2c_Write_Byte(int fd, __u8 address, __u8 value)
+{
+   if (i2c_smbus_write_byte_data(fd, address, value) < 0) {
+      close(fd);
+      exit(1);
+   }
+}
 
-	sketch[2*pen_position[0]] = sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
-	bus.write_i2c_block_data(matrix, 0, sketch)
+void i2c_Read_Block(int fd, __u8 address, __u8 length, __u8 *values)
+{
+   if(i2c_smbus_read_i2c_block_data(fd, address,length,values)<0) {
+      close(fd);
+      exit(1);
+   }
+}
+
+int i2c_Begin()
+{
+   int fd;
+   char *fileName = "/dev/i2c-0";
+   
+   // Open port for reading and writing
+   if ((fd = open(fileName, O_RDWR)) < 0)
+      exit(1);
+   
+   // Set the port options and set the address of the device
+   if (ioctl(fd, I2C_SLAVE, BMP085_I2C_ADDRESS) < 0) {               
+      close(fd);
+      exit(1);
+   }
+
+   return fd;
+}
+
+void matrix_Calibration()
+{
+	i2c_Begin();
+	
+	i2c_Write_Byte(fd,matrix,0x21);
+	i2c_Write_Byte(fd,matrix,0x81);
+	i2c_Write_Byte(fd,matrix,0x81);
+}
+
+int main(int argc, char **argv) {
+	matrix_Calibration();
+	i2c_Begin();
+	
+	sketch[2*pen_position[0]] = sketch[2*pen_position[0]] | (1<<(8-pen_position[1]));
+	i2c_Write_Byte(fd, matrix, sketch);
 
 	while(true){
 		if (action == "up" and pen_position[1] < 8):
-			pen_position = [pen_position[0], pen_position[1]+1]
+			pen_position = [pen_position[0], pen_position[1]+1];
 		if (action == "down" and pen_position[1] > 1):
-			pen_position = [pen_position[0], pen_position[1]-1]
+			pen_position = [pen_position[0], pen_position[1]-1];
 		if (action == "left" and pen_position[0] < 8-1):
-			pen_position = [pen_position[0]+1, pen_position[1]]
+			pen_position = [pen_position[0]+1, pen_position[1]];
 		if (action == "right" and pen_position[0] > 0):
-			pen_position = [pen_position[0]-1, pen_position[1]]
+			pen_position = [pen_position[0]-1, pen_position[1]];
 		if (action == "shake"):
-			sketch = [0x00 for i in range(16)]
+			sketch = [0x00 for i in range(16)];
 
-		sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
-		bus.write_i2c_block_data(matrix, 0, sketch)
+		sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]));
+		i2c_Write_Byte(fd, matrix, sketch);
 	}
 			
 	return 0;
