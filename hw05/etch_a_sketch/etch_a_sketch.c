69857b0aafb8036d6fb3a84fae562c666c3ef6d0
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-11 09:38:18 -0400

    Update etch_a_sketch.c

--- hw05/etch_a_sketch/etch_a_sketch.c
+++ hw05/etch_a_sketch/etch_a_sketch.c
@@ -10,20 +10,15 @@ import smbus
 #include <stdint.h>
 #include <fcntl.h>
 #include <stdlib.h>
-
-#include <linux/input.h>	/* BUS_I2C */
-#include <linux/i2c.h>
-#include <linux/module.h>
-#include <linux/of.h>
-#include <linux/types.h>
-#include <linux/pm.h>
-#include "adxl34x.h"
-
 #include <unistd.h>
 #include <linux/i2c-dev.h>
 #include <linux/i2c.h>
 #include <sys/ioctl.h>
+
+#include "smbus.c" 
 #include "smbus.h" 
+#include "adxl34x.c"
+#include "adxl34x.h"
 
 
 MODULE_LICENSE("GPL");
@@ -40,30 +35,25 @@ bus.write_byte_data(matrix, 0x21, 0)
 bus.write_byte_data(matrix, 0x81, 0)
 bus.write_byte_data(matrix, 0xe7, 0)
 
-sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
+sketch[2*pen_position[0]] = sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
 bus.write_i2c_block_data(matrix, 0, sketch)
 
-@app.route("/")
-def index():
-	return render_template('index.html')
+int main(int argc, char **argv) {
+	while(true){
+		if (action == "up" and pen_position[1] < 8):
+			pen_position = [pen_position[0], pen_position[1]+1]
+		if (action == "down" and pen_position[1] > 1):
+			pen_position = [pen_position[0], pen_position[1]-1]
+		if (action == "left" and pen_position[0] < 8-1):
+			pen_position = [pen_position[0]+1, pen_position[1]]
+		if (action == "right" and pen_position[0] > 0):
+			pen_position = [pen_position[0]-1, pen_position[1]]
+		if (action == "shake"):
+			sketch = [0x00 for i in range(16)]
 
-@app.route("/<action>")
-def action(action):
-	global pen_position
-	global sketch
-
-	if (action == "up" and pen_position[1] < 8):
-		pen_position = [pen_position[0], pen_position[1]+1]
-	if (action == "down" and pen_position[1] > 1):
-		pen_position = [pen_position[0], pen_position[1]-1]
-	if (action == "left" and pen_position[0] < 8-1):
-		pen_position = [pen_position[0]+1, pen_position[1]]
-	if (action == "right" and pen_position[0] > 0):
-		pen_position = [pen_position[0]-1, pen_position[1]]
-	if (action == "shake"):
-		sketch = [0x00 for i in range(16)]
+		sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
+		bus.write_i2c_block_data(matrix, 0, sketch)
+	}
 			
-	sketch[2*pen_position[0]]=sketch[2*pen_position[0]] | (1<<(8-pen_position[1]))
-	bus.write_i2c_block_data(matrix, 0, sketch)
-			
-	return render_template('index.html')
+	return 0;
+}
