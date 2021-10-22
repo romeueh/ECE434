1ed31489d2643b2ede560f4fceb087c370b85b4d
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-22 16:25:18 -0400

    Create setup.sh

---
+++ hw07/setup.sh
@@ -0,0 +1,8 @@
+#define BLYNK_TEMPLATE_ID "TMPLBNJZ_xD6"
+#define BLYNK_DEVICE_NAME "Beagle Bone Connection"
+#define BLYNK_AUTH_TOKEN "P7cwZgK-waBwdYRPwry9P-UXZ_OoMYDR"
+
+export BLYNK_AUTH_TOKEN='P7cwZgK-waBwdYRPwry9P-UXZ_OoMYDR'
+
+I2C=/sys/class/i2c-adapter/i2c-2
+echo temp101 0x77 > $I2C/new_device
