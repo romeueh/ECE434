53b365f9a20818d3fd824b29d15f62d64ca13ac7
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-25 12:28:12 -0400

    Update setup.sh

--- hw07/setup.sh
+++ hw07/setup.sh
@@ -1,3 +1,7 @@
+echo i2c1: P9_24, P9_26
+config-pin P9_24 i2c 
+config-pin P9_26 i2c
+
 #define BLYNK_TEMPLATE_ID "TMPLBNJZ_xD6"
 #define BLYNK_DEVICE_NAME "Beagle Bone Connection"
 #define BLYNK_AUTH_TOKEN "P7cwZgK-waBwdYRPwry9P-UXZ_OoMYDR"
