59df12707b76cc0cd98fed69ee4efadc180f7f41
romeueh <75344382+romeueh@users.noreply.github.com>
2021-10-31 21:03:11 -0400

    Update pwm1.pru0.c

--- hw08/pwm1.pru0.c
+++ hw08/pwm1.pru0.c
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <pru_cfg.h>
 #include "resource_table_empty.h"
-#include "PruGpio.h"
+#include "pruGpio.h"
 
 volatile register uint32_t __R30;
 volatile register uint32_t __R31;
